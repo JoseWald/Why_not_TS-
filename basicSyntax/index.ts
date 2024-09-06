228713458c9ddee81adf8d5a6004fc99b01ff9c4
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-09-06 15:22:19 +0300

    custom types

--- basicSyntax/index.ts
+++ basicSyntax/index.ts
@@ -1,3 +1,20 @@
+//variable declaration
 let myName:string="Jose" 
 let myAge:number = 19
 let isStudent:boolean = true
+
+//custom types
+type Food = string
+let favFood: Food = "rice"
+
+type Person={
+    name: string,
+    age : number,
+    isStudent: boolean
+}
+
+let person  :  Person={
+    name:"Jose",
+    age:18,
+    isStudent:true
+}
