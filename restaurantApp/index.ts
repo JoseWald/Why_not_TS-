39094acb6c5bbf53c3e483749408a5a677af794b
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-09-06 10:30:06 +0300

    you see, all the const error is in red

---
+++ restaurantApp/index.ts
@@ -0,0 +1,39 @@
+const menu=[
+    {name:"Margherita" , price:8},
+    {name:"Pepperroni" , price:10 },
+    {name:"Hawaiian" , price:10 },
+    {name:"Veggie" , price:9 }
+]
+
+const cashInRegister=100
+const orderQueue=[]
+let nextOrderId=1;
+
+const addPizza=(name , price)=>{
+    menu.push({name:name, price: price})
+
+}
+
+const placeOrder=(name)=>{
+     const order=menu.find(pizza=>pizza.name==name)
+     orderQueue.push({pizza:order, statut:"Ordered",Id:nextOrderId++})
+     cashInRegister+=order.price
+     return order
+}
+
+const completeOrder=(Id)=>{
+    const order=orderQueue.find(order=>order.Id==Id)
+    order.statut="Completed"
+    return order
+}
+
+addPizza("Chiken bacon ranch",12)
+addPizza("BBQ Chicken",12)
+addPizza("Spicy sausage",11)
+
+placeOrder("BBQ Chicken")
+completeOrder("1")
+
+console.log("Menu:"+menu)
+console.log("Cash In Register:"+ cashInRegister)
+console.log("order Queue:"+orderQueue)
