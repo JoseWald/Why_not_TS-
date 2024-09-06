10ce3f5fd91e93bace31f25d7c773a6ac01d7d17
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-09-06 14:47:26 +0300

    solving the bug

--- restaurantApp/index.ts
+++ restaurantApp/index.ts
@@ -1,12 +1,14 @@
-const menu=[
+let menu=[
     {name:"Margherita" , price:8},
     {name:"Pepperroni" , price:10 },
     {name:"Hawaiian" , price:10 },
     {name:"Veggie" , price:9 }
 ]
 
-const cashInRegister=100
-const orderQueue=[]
+let cashInRegister=100
+let orderQueue=[
+    {pizza:"name of pizza", statut:"Ordered/Completed" , Id:0}
+]
 let nextOrderId=1;
 
 const addPizza=(name , price)=>{
@@ -16,14 +18,18 @@ const addPizza=(name , price)=>{
 
 const placeOrder=(name)=>{
      const order=menu.find(pizza=>pizza.name==name)
-     orderQueue.push({pizza:order, statut:"Ordered",Id:nextOrderId++})
-     cashInRegister+=order.price
+     orderQueue.push({pizza:String(order), statut:"Ordered",Id:nextOrderId++})
+     if(order)
+        cashInRegister+=order.price
      return order
 }
 
 const completeOrder=(Id)=>{
     const order=orderQueue.find(order=>order.Id==Id)
-    order.statut="Completed"
+    if(order){
+           order.statut="Completed"
+    }
+ 
     return order
 }
 
@@ -37,3 +43,11 @@ completeOrder("1")
 console.log("Menu:"+menu)
 console.log("Cash In Register:"+ cashInRegister)
 console.log("order Queue:"+orderQueue)
+
+/*
+    TypeScript may intimidate you with all the code underlined in red
+    but it will help you to keep your code maintainable
+
+    The first time will be a little hard but practice will make you a master
+    
+*/
