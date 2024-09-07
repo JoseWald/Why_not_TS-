4dcfc994c2c7a9cff1d502de6ab6902774fe0352
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-09-07 11:07:55 +0300

    finished the nested Object types and array declaration

--- basicSyntax/index.ts
+++ basicSyntax/index.ts
@@ -1,7 +1,7 @@
 //variable declaration
-let myName:string="Jose" 
-let myAge:number = 19
-let isStudent:boolean = true
+let myName: string = "Jose" 
+let myAge : number = 19
+let isStudent : boolean = true
 
 //custom types
 type Food = string
@@ -20,3 +20,36 @@ let person  :  Person={
 }
 
 //Nested Object types
+type Person1={
+    name: string,
+    age : number,
+    isStudent: boolean
+    adress:{
+        street:string,
+        city:string,
+        country:string
+    }
+
+}
+
+let Jean :Person1={
+    name:"Jose",
+    age:18,
+    isStudent:true,
+    adress:{
+        street:"43",
+        city:"Antananarivo",
+        country:"Madagascars"
+    }
+
+}
+
+function displayInfo(person : Person1){
+    console.log(`${person.name} lives at ${person.adress.street} of ${person.adress.city} ${person.adress.country}`)
+}
+
+displayInfo(Jean)
+
+//Array declarations
+let personList :Person1[] =[Jean]
+let people: Array<Person1>=[Jean]
